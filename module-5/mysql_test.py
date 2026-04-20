776ce909d4308f77e167702b80311af85e3c81fc
Kris Kleiner <kmkleiner@gmail.com>
2026-04-19 21:22:09 -0500

    Module 5 Movies Setup assignment

---
+++ module-5/mysql_test.py
@@ -0,0 +1,44 @@
+# Kris Kleiner
+# Date: April 19, 2026
+# Assignment: Movies Setup
+
+import mysql.connector
+from mysql.connector import errorcode
+from dotenv import dotenv_values
+
+# load .env file
+secrets = dotenv_values(".env")
+
+# database config
+config = {
+    "user": secrets["USER"],
+    "password": secrets["PASSWORD"],
+    "host": secrets["HOST"],
+    "database": secrets["DATABASE"],
+    "raise_on_warnings": True
+}
+
+db = None
+
+try:
+    db = mysql.connector.connect(**config)
+
+    print(
+        "\nDatabase user {} connected to MySQL on host {} with database {}".format(
+            config["user"], config["host"], config["database"]
+        )
+    )
+
+    input("\nPress any key to continue...")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("The supplied username or password are invalid")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("The specified database does not exist")
+    else:
+        print(err)
+
+finally:
+    if db is not None and db.is_connected():
+        db.close()
